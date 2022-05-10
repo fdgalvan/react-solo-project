b3eb04cd0ecdafa2d789cb46d1665ad003270f3f
Felipe AKA Chewie <99052148+fdgalvan@users.noreply.github.com>
2022-05-10 02:23:58 -0500

    API is being accessed, info is showing up on screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
-  const [movies, setMovies] = useState([])
-  return (
-    <div>App</div>
-  )
-}
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-export default App
+	const getMovieRequest = async (searchValue) => {
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=bbd297e5`
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+		
+		if(responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() =>{
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieList movies={movies} />
+			</div>
+		</div>
+	);
+};
+
+export default App;
